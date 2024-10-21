d9875becc9c9ee8ca1edc7d706e5d619db3fb104
kuangshen04 <2832899707@qq.com>
2024-10-21 23:24:50 +0800

    拆分help menu

---
+++ Views/HelpMenuWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Wukong_PBData_ReadWriter_GUI.Views;
+
+public partial class HelpMenuWindow : Window
+{
+    public HelpMenuWindow()
+    {
+        InitializeComponent();
+        if (File.Exists("README.md"))
+        {
+            HelpTextBlock.Text = File.ReadAllText("README.md");
+        }
+    }
+
+    private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+    {
+        System.Diagnostics.Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+        e.Handled = true;
+    }
+}
